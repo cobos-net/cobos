ecf79a85e50b8f58283c6d268252163fbcfe3562
ndavis <ndavis@VEA795Dw3.ingrnet.com>
2011-05-02 08:28:31 +1000

    Re-organised pre build for projects.

---
+++ DEV/Code/Intergraph.AsiaPac.Data.Tests/BUILD/prebuild.bat
@@ -0,0 +1,45 @@
+@echo off
+REM ===========================================================================
+REM Filename: prebuild.bat
+REM Description: runs xsd.exe to generate C# classes
+REM ---------------------------------------------------------------------------
+REM Created by:                 Date:
+REM Updated by:                 Date:
+REM ---------------------------------------------------------------------------
+REM Notes: Requires 1 parameter to set working directory to project
+REM directory so that the relative paths work.
+REM
+REM ===========================================================================
+
+cd %1
+
+REM Build tools & processing stylesheets
+REM ===========================================================================
+
+set stylesheets=..\Intergraph.AsiaPac.Data\Stylesheets
+set schemas=.\Schemas
+
+set xsd="C:\Program Files\Microsoft SDKs\Windows\v6.0A\bin\xsd.exe"
+set xsltjs=%stylesheets%\xslt.js
+set db2xsd="\Projects\Intergraph.AsiaPac.DevTools\release build folder\db2xsd\db2xsd.exe"
+
+REM Input data
+REM ===========================================================================
+
+
+REM Output folders
+REM ===========================================================================
+
+@echo on
+
+echo ==========================================================================
+echo 1. Pre-process database schemas
+echo ==========================================================================
+
+echo 1.1 Refresh the Xslt database variables
+echo ------------------------------------
+%db2xsd% /schema:eadev /output:%stylesheets%\CadDatabase.xsd AEVEN EVENT EVCOM CD_UNITS UN_HI DISPASS_EVENT EV_DISPO DIVERT INCIDENT_TRACKING VEHIC PERSO TOW_VEHIC CONT_NAME PROPT
+
+echo 1.2 Convert the database schema to Xslt variables
+echo ------------------------------------
+%xsltjs% %stylesheets%\CadDatabase.xsd %stylesheets%\DatabaseTypes.xslt %stylesheets%\CadDatabase.xslt
