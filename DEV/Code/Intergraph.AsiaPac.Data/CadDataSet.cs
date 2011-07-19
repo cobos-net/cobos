4cdbfc482b00dd03948f814a90c62d0e3442074f
ndavis <ndavis@VEA795Dw3.ingrnet.com>
2011-07-19 15:55:30 +1000

    First pass of refactoring for Intergraph branding.

--- DEV/Code/Intergraph.AsiaPac.Data/CadDataSet.cs
+++ DEV/Code/Intergraph.AsiaPac.Data/CadDataSet.cs
@@ -1,17 +1,65 @@
-﻿using System;
+﻿// ============================================================================
+// Filename: CobosDataSet.cs
+// Description: 
+// ----------------------------------------------------------------------------
+// Created by: N.Davis                          Date: 27-Nov-09
+// Modified by:                                 Date:
+// ============================================================================
+// Copyright (c) 2009-2011 Nicholas Davis		nick@cobos.co.uk
+//
+// Cobos Software Development Kit v0.1
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ============================================================================
+
+// 05-Feb-11 N.Davis
+// -----------------
+// Rebranded from "Cobos" to "Intergraph.AsiaPac" in preparation for use in the Generic CAD Interoperability project
+
+using System;
 using System.Data;
 using System.Xml;
 using System.Xml.Xsl;
 using System.IO;
 using System.Text;
 using System.Diagnostics;
+
+#if INTERGRAPH_BRANDING
 using Intergraph.AsiaPac.Utilities.Xml;
 
 namespace Intergraph.AsiaPac.Data
+#else
+using Cobos.Utilities.Xml;
+
+namespace Cobos.Data
+#endif
 {
+#if INTERGRAPH_BRANDING
 	using AsyncDBTask = AsyncTask<CadDataSet, DatabaseAdapter.QueryDatabaseAsync>;
 
 	public class CadDataSet : DataSet
+#else
+	using AsyncDBTask = AsyncTask<CobosDataSet, DatabaseAdapter.QueryDatabaseAsync>;
+
+	public class CobosDataSet : DataSet
+#endif
 	{
 		/// <summary>
 		/// 
@@ -38,7 +86,11 @@ namespace Intergraph.AsiaPac.Data
 		/// 
 		/// </summary>
 		/// <param name="dataSetName"></param>
+#if INTERGRAPH_BRANDING
 		public CadDataSet( string dataSetName )
+#else
+		public CobosDataSet( string dataSetName )
+#endif
 			: base( dataSetName )
 		{
 		}
@@ -47,7 +99,11 @@ namespace Intergraph.AsiaPac.Data
 		/// 
 		/// </summary>
 		/// <param name="dataSet"></param>
+#if INTERGRAPH_BRANDING
 		public CadDataSet()
+#else
+		public CobosDataSet()
+#endif
 			: base()
 		{
 		}
--- DEV/Code/Intergraph.AsiaPac.Web.Utilities/Handlers/XmlCompressionModule.cs
+++ DEV/Code/Intergraph.AsiaPac.Web.Utilities/Handlers/XmlCompressionModule.cs
@@ -1,3 +1,38 @@
+// ============================================================================
+// Filename: XmlCompressionModule.cs
+// Description: 
+// ----------------------------------------------------------------------------
+// Created by: N.Davis                          Date: 27-Nov-09
+// Modified by:                                 Date:
+// ============================================================================
+// Copyright (c) 2009-2011 Nicholas Davis		nick@cobos.co.uk
+//
+// Cobos Software Development Kit v0.1
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ============================================================================
+
+// 05-Feb-11 N.Davis
+// -----------------
+// Rebranded from "Cobos" to "Intergraph.AsiaPac" in preparation for use in the Generic CAD Interoperability project
+
 using System;
 using System.Data;
 using System.Configuration;
@@ -10,7 +45,11 @@ using System.Web.UI.HtmlControls;
 using System.IO.Compression;
 using System.Globalization;
 
+#if INTERGRAPH_BRANDING
 namespace Intergraph.AsiaPac.Web.Utilities.Handlers
+#else
+namespace Cobos.Web.Utilities.Handlers
+#endif
 {
 	public class XmlCompressionModule : IHttpModule
 	{
