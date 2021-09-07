caa35a30e79cc996beb5d5d46a8f217b9fbe125c
Nick Davis <nicholas.davis@hexagonsi.com>
2021-09-07 16:29:10 +1000

    First refactor for .NET SDK projects

---
+++ DEV/src/Cobos/Utilities/Extensions/ObjectExtensions.cs
@@ -0,0 +1,95 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="ObjectExtensions.cs" company="Cobos SDK">
+//
+//      Copyright (c) 2009-2014 Nicholas Davis - nick@cobos.co.uk
+//
+//      Cobos Software Development Kit
+//
+//      Permission is hereby granted, free of charge, to any person obtaining
+//      a copy of this software and associated documentation files (the
+//      "Software"), to deal in the Software without restriction, including
+//      without limitation the rights to use, copy, modify, merge, publish,
+//      distribute, sublicense, and/or sell copies of the Software, and to
+//      permit persons to whom the Software is furnished to do so, subject to
+//      the following conditions:
+//      
+//      The above copyright notice and this permission notice shall be
+//      included in all copies or substantial portions of the Software.
+//      
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//      NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//      LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//      OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//      WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Cobos.Utilities.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// Extension methods for the <see cref="Object"/> class.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Serialize the object (usually a struct with StructLayout attributes)
+        /// </summary>
+        /// <param name="self">The 'this' object reference.</param>
+        /// <returns>The bytes representing the object.</returns>
+        public static byte[] ConvertToByteArray(this object self)
+        {
+            int size = Marshal.SizeOf(self);
+            IntPtr buffer = Marshal.AllocHGlobal(size);
+
+            try
+            {
+                Marshal.StructureToPtr(self, buffer, false);
+                byte[] data = new byte[size];
+                Marshal.Copy(buffer, data, 0, size);
+                return data;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffer);
+            }
+        }
+
+        /// <summary>
+        /// Convert the bytes to an object type.
+        /// </summary>
+        /// <typeparam name="T">The type of object to convert to.</typeparam>
+        /// <param name="self">The 'this' object reference.</param>
+        /// <returns>The object result.</returns>
+        public static T ConvertTo<T>(this byte[] self)
+        {
+            Type type = typeof(T);
+
+            int size = Marshal.SizeOf(type);
+
+            if (size > self.Length)
+            {
+                return default(T);
+            }
+
+            IntPtr buffer = Marshal.AllocHGlobal(size);
+
+            try
+            {
+                Marshal.Copy(self, 0, buffer, size);
+
+                return (T)Marshal.PtrToStructure(buffer, type);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffer);
+            }
+        }
+    }
+}
