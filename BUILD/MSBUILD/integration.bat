202179db984d42b3030b7bcd9e070a102b42cb69
Nick Davis <nicholas.davis@hexagonsi.com>
2021-09-15 17:51:41 +1000

    Second pass of restructure for NuGet

--- BUILD/MSBUILD/integration.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call \QualitySystem\BUILD\MSBUILD\autobuild.bat "%~dp0\package.msbuild" Integration "%~dp0\build.state"
--- BUILD/MSBUILD/production.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call \QualitySystem\BUILD\MSBUILD\autobuild.bat "%~dp0\package.msbuild" Production "%~dp0\build.state"
